fa3ff0b171fdc0175603b12fa081e2b57e64af00
paigechampagne <paigechamp@gmail.com>
2019-09-17 01:43:09 -0500

    update, almost finished, just need to comment

--- assignments/P01/list.cpp
+++ assignments/P01/list.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#include <string>   
-#include <fstream> 
-
+#include <string>
+#include <fstream>
 
 using namespace std;
 
 //struct of nodes for the list
 
-struct empdata{
-    string emp_id; 
+struct empdata
+{
+    string emp_id;
     string first_name;
     string last_name;
     string gender;
@@ -16,7 +16,8 @@ struct empdata{
     double hourly_pay;
     empdata *Next;
 
-    empdata(){
+    empdata()
+    {
         emp_id = "";
         first_name = "";
         last_name = "";
@@ -24,7 +25,8 @@ struct empdata{
         email = "";
         hourly_pay = 0.0;
     }
-    empdata(string id,string first,string last,string sex,string mail, double pay){
+    empdata(string id, string first, string last, string sex, string mail, double pay)
+    {
         emp_id = id;
         first_name = first;
         last_name = last;
@@ -34,174 +36,187 @@ struct empdata{
     }
 };
 
-/*struct Node{
-    empdata data;
-    Node* Next = NULL; //creates pointer Next
-    Node(empdata a){
+bool substringMatch(string macro, string micro)
+{
+    return (macro.find(micro) != std::string::npos);
+};
 
-        data = a;
-        Next = NULL;
-    }
-}; */
+class LinkedList
+{
+    empdata *Head; //creates node called Head that will point to the first node
+    empdata *Tail;
 
-void menu(){
-    int choice;
-        cout<<"\nMenu of Operations:\n";
-        cout<<"    1 - Find Employee\n";
-        cout<<"    2 - Delete Employee\n";
-        cout<<"    3 - Quit\n";
-        cin >> choice;
-    switch(choice)
-    {
-        case 1: void find(string micro, empdata a);
-            break;
-       // case 2: void delete();
-            break;
-        case 3:
-            break;
-    }
-}
-
-class LinkedList{
-    empdata* Head; //creates node called Head that will point to the first node
-    empdata* Tail;
 public:
-    LinkedList(){
+    LinkedList()
+    {
         Head = NULL; //sets Head to null bc it's not pointing to anything
         Tail = NULL;
     }
-  
-  void push(string id,string first,string last,string sex,string mail, double pay){
-        
-        empdata* Temp = new empdata(id, first, last, sex, mail, pay); //creates new node called Temp
 
-        if(Head == NULL){ //if Head isn't pointing to anything
+    void push(string id, string first, string last, string sex, string mail, double pay)
+    {
+
+        empdata *Temp = new empdata(id, first, last, sex, mail, pay); //creates new node called Temp
+
+        if (Head == NULL)
+        {                //if Head isn't pointing to anything
             Head = Temp; //assigns temp to be the first node
-        }else{              //if Head is pointing to something,
-            Temp->Next = Head; //makes Temp's next value equal Head?? does it make Head null??
-            Head = Temp; //Head equals temp
         }
-  };
+        else
+        {                      //if Head is pointing to something,
+            Temp->Next = Head; //makes Temp's next value equal Head?? does it make Head null??
+            Head = Temp;       //Head equals temp
+        }
+    };
 
-void pop(string id,string first,string last,string sex,string mail, double pay){
-   empdata* Temp = Head;//creates new node called Temp
-   Head = Temp->Next;
-   delete Temp;
-  // cout <<Head->emp_id<<", "<<Head->first_name<<", "<<Head->last_name<<", "
-//       <<Head->email<<", "<<Head->gender<<", "<<Head->hourly_pay<<endl;
+    void pop(string id, string first, string last, string sex, string mail, double pay)
+    {
+        empdata *Temp = Head; //creates new node called Temp
+        Head = Temp->Next;
+        delete Temp;
+    };
 
-};
-void del(string id){
-   empdata* Temp = Head;//creates new node called Temp
-   empdata* del;
-    string macro, micro;
-    cout << "Enter the ID of the employee you wish to delete: ";
-    cin >> micro;
-  do{
-   Head = Temp; 
-   cout << Head->emp_id << "\n";
-     del = Temp->Next;
-     cout << del->emp_id << "\n";
-    Temp = Temp->Next->Next;
-    cout << Temp->emp_id << "\n";
-    macro = del->emp_id;
-    if(Temp->Next=NULL){
-        cout << "Employee not found\n";
-    }
-  }while(!macro.find(micro) != std::string::npos);
-    cout << "Out of loop\n";
-    delete del;
-   
-};
-   
+    void del(string id)
+    {
+        empdata *Temp = Head; //creates new node called Temp
+        empdata *del;
+        string macro, micro;
+        cout << "Enter the ID of the employee you wish to delete: ";
+        cin >> micro;
+        do
+        {
+            if (substringMatch(macro, micro) == 1)
+            {
+                del = Temp->Next;
+                macro = del->emp_id;
+                Temp = Temp->Next;
+                delete del;
+                cout << "Deleted\n";
+            }
+            else
+            {
+                del = Temp->Next;
+                macro = del->emp_id;
+                Temp = Temp->Next->Next;
+            };
 
+        } while (Temp->Next != NULL);
 
+        if (substringMatch(macro, micro) == 0)
+        {
+            cout << "Not found.\n";
+        }
+            
+    };
 
-empdata find(string email){
-   empdata* Temp = Head;
-   empdata* Result = Head;
-    string macro, micro;
-    cout << "Which email address are you looking for? ";
-    cin >> micro;
-    
-    do{
-        
-        macro = Temp->email;
-        Result = Temp;
-        Temp = Temp->Next;
-         if (Temp->Next=NULL){
-             cout << "Not found\n";
-         }
-         
-    }while(!macro.find(micro) != std::string::npos);
-    return *Result;
-};
+    void printid(string id /*,string first,string last,string sex,string mail, double pay*/)
+    {
+        ofstream fout;
+        fout.open("output.dat",std::ios::app);
+        empdata *Temp = Head;
 
-/*bool find(string email){
-    empdata* Temp = Head;
-   bool result= 0;
-    string macro, micro;
-    cout << "Which email address are you looking for? ";
-    cin >> micro;
-    
-    do{
-        
-        macro = Temp->email;
-        Temp = Temp->Next;
-        if (macro.find(micro) != std::string::npos){
-             cout << "Found!\n";
-            result = 1;
-         }
-   }while(Temp->Next!=NULL);
-
-   return result;
-   
-};*/
-
-
- void printid(string id /*,string first,string last,string sex,string mail, double pay*/){
-      empdata* Temp = Head;
-
-        for(int i = 0; i < 30; i++){
-            cout<<Temp->emp_id<<"\n";
-            if(Temp->Next){
-             Temp = Temp-> Next;
+        for (int i = 0; i < 30; i++)
+        {
+            fout << Temp->emp_id << "\n";
+            if (Temp->Next)
+            {
+                Temp = Temp->Next;
             };
         };
-            
-     };
-};
-
-
-
-int main(){
- 
- LinkedList EmpList;
-
-    string empid;
-    string first;
-    string last;
-    string email;
-    string gender;
-    double pay;
-
-    string line;
-    string *tokens;
-    ifstream fin("employees.dat");
-
-
-    while(!fin.eof()){
-
-        fin>>empid>>first>>last>>email>>gender>>pay;
-
-       //cout<<empid<<", "<<first<<", "<<last<<", "<<email<<", "<<gender<<", "<<pay<<endl;
-
-        EmpList.push(empid,first,last,email,gender,pay);
+              fout.close();
     };
-   //cout << EmpList.find(email).email;
-   //EmpList.pop(empid,first,last,email,gender,pay);
-    //EmpList.printid(empid);
-    EmpList.del(empid);
+     bool find(string email){
+            ofstream fout;
+            fout.open("output.dat",std::ios::app);
+           
+            empdata *Temp = Head;
+            empdata *Result = Head;
+            bool found = false;
+            string macro, micro;
+            cout << "Which email address are you looking for? ";
+            cin >> micro;
 
-return 0;
+            do
+            {
+
+                macro = Temp->email;
+                Result = Temp;
+                Temp = Temp->Next;
+                if (macro.find(micro) != std::string::npos)
+                {
+                    found = true;
+                    fout << found << "\n";
+                }
+            } while (Temp->Next != NULL);
+            return found;   
+        };
+    
 };
+
+    int main()
+    {
+        LinkedList EmpList;
+
+        string empid;
+        string first;
+        string last;
+        string email;
+        string gender;
+        double pay;
+
+        string line;
+        string *tokens;
+        ifstream fin("employees.dat");
+
+        bool menu = 1;
+
+        while (!fin.eof())
+        {
+
+            fin >> empid >> first >> last >> email >> gender >> pay;
+
+            //cout<<empid<<", "<<first<<", "<<last<<", "<<email<<", "<<gender<<", "<<pay<<endl;
+
+            EmpList.push(empid, first, last, email, gender, pay);
+        };
+
+        while (menu == true)
+        {
+           
+            int choice;
+            cout << "\nMenu of Operations:\n";
+            cout << "    1 - Find Employee\n";
+            cout << "    2 - Delete Employee\n";
+            cout << "    3 - Push Employee\n";
+            cout << "    4 - Pop Employee\n";
+            cout << "    5 - Print Employee IDs\n";
+            cout << "    6 - Quit\n";
+            cin >> choice;
+            switch (choice)
+            {
+            case 1:
+                EmpList.find(email);
+                break;
+            case 2:
+                EmpList.del(empid);
+                break;
+            case 3:
+                EmpList.push(empid, first, last, email, gender, pay);
+                cout << "Done\n";
+                break;
+            case 4:
+                EmpList.pop(empid, first, last, email, gender, pay);
+                cout << "Done\n";
+                break;
+            case 5:
+                EmpList.printid(empid);
+                break;
+            case 6:
+                cout << "Goodbye\n";
+                menu = 0;
+                break;
+            }
+        };
+        
+        return 0;
+    }
